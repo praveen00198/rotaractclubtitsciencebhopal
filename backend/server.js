9632d8ee919fe8dfd0fd2f2fbdb354ef70e6b475
Praveen Patel <praveenpatel5957@gmail.com>
2025-11-04 22:39:33 +0530

    Again

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const event = require("./routes/eventroute");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://rotaractcluboftitandscience.netlify.app"], 
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
